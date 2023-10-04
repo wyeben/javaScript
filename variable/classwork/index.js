c6681aa60aff99b5c883fba46d3228d6e72dd804
Wyeben <yila.ben@yahoo.com>
2023-10-04 07:48:52 +0100

    exercise

---
+++ variable/classwork/index.js
@@ -0,0 +1,32 @@
+var id = "as";
+var available = "as";
+var count = "as";
+var name = "as";
+var arthor = "as"
+
+id = 2;
+available = true;
+count = 2;
+name = ""
+arthor = ""
+
+var num = 1;
+if (num == 1)
+   num = 0;
+else  num == 0
+    num = 1;
+if(num == 0)
+   num = 1;
+else num == 1
+   num = 0;
+
+let value = 1;
+value = value == 1 ? 0 : value == 0 ? 1 : value == 1 ? 0 : 1;
+
+console.log(value)
+// console.log(num)
+// console.log(typeof id)
+// console.log(typeof available)
+// console.log(typeof count)
+// console.log(typeof name)
+// console.log(typeof arthor )
---
+++ variable/index.js
@@ -0,0 +1,14 @@
+var1 = 10;
+var2 = 20;
+var3 = 30;
+
+
+var4 = var1
+var1 = var2
+var2 = var3
+var3 = var4
+
+console.log(var1)
+console.log(var2)
+console.log(var3)
+
